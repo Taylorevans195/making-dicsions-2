14412e95ec2a4e2d50c03b8fc4a78e12c5af30b1
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2023-03-23 14:00:58 -0600

    Problem-8

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -124,6 +124,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 //Code Here
 
 
+for(i = 0; i < listOfNumbers.length; i++){
+  if (listOfNumbers[i] % 3 === 0)
+  console.log( listOfNumbers[i] + " is divisible by 3.")
+}
+
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
 var letters = ['A', 'B', 'C', 'D', 'E'];
